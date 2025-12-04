285ea0890dffab3ded2e32fd30f87df70a7f9349
SamuelMaldonado023 <Samuel.maldonado3@upr.edu>
2025-12-04 22:49:54 +0000

    new file:   template 	modified:   template.cpp

--- template.cpp
+++ template.cpp
@@ -34,11 +34,30 @@ int readFiles(const string& directory) {
     return counter;
 }
 
+int option;
+string userkeys,top1,top2,top3,top4,top5;
+
 int main() {
    
     // How you send the file name dependes of where your executable file is
-    int amount_of_files = readFiles("../books_description_files");
-    cout << "The amount of files in directory is: " << amount_of_files << "\n";
+    //int amount_of_files = readFiles("books_description_files");
+    //cout << "The amount of files in directory is: " << amount_of_files << "\n";
+
+    cout<< "\n----Description Search Engine-----\n
+            \tSearch movies and books descriptions by keywords
+            \tEnter keywords:" ;
+
+    cin >> userkeys;
+
+    // if nothing with that keyword found, desplegar failure or empty, 
+    // si encuentran related keywords
+     cout<< "\n----Top 5-----\n
+            \t" << top1 << endl << top2 << endl << top3 << endl << top4 << endl << top5 << endl;
+     cout << "Choose from 1-5, 0 to quit";
+     cin << option;
+     if (option == 0) break;
+     else // display filename chosen and description
+            
 
     return 0;
 }
